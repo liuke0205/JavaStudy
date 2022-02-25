1a28951ad100a4cbd8ef4927fb1ae59bad38d680
liuke990205 <liuke990205@126.com>
2022-02-26 00:23:32 +0800

    feat(Leetcode):增加题目

---
+++ DataStructure/src/Topology.java
@@ -0,0 +1,44 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author liuke
+ * @date 2022/2/26 0:12
+ */
+public class Topology {
+    // 力扣207 课程表 https://leetcode-cn.com/problems/course-schedule/
+    // 使用拓扑排序检测是否存在循环依赖
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[][] graph = new int[numCourses][numCourses];
+        int[] indeg = new int[numCourses];
+        // 构造邻接矩阵
+        for (int i = 0; i < prerequisites.length; i++) {
+            int form = prerequisites[i][0];
+            int to = prerequisites[i][1];
+            graph[form][to] = 1;
+            indeg[to]++;
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        // 将入读为0的节点入队列
+        for (int i = 0; i < indeg.length; i++) {
+            if (indeg[i] == 0){
+                queue.add(i);
+            }
+        }
+        int num = 0; //记录没有环的个数
+        while (!queue.isEmpty()){
+            Integer from = queue.poll();
+            num ++;
+            //删除相关的边，如果遇到入读为0，直接入队
+            for (Integer to: graph[from]) {
+                graph[from][to] = 0;
+                indeg[to]--;
+                if (indeg[to] == 0){
+                    queue.add(to);
+                }
+            }
+        }
+        return num == numCourses;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # JavaStudy
+
+> 本项目主要记录学习Java的历程。
+
+## Concurrent
+
+> 这里记录Java并发学习的代码
+
+## DataStructure
+
+> 这里记录常见的数据结构实现代码
+
+* 循环队列
+* LRU算法
+* 优先队列
+* 跳表
+* 前缀树
+* 并查集
+* 差分数组
+
+## DesignPattern
+
+
+## Leetcode
+
+
+
+## MySource
+
+
+## RocketMQ
+
+
+## SortAlgorithm
+
+
+## Zookeeper
