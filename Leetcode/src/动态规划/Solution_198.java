cd2f557573233b754fc35d2d0949fdd8a5950b71
liuke990205 <liuke990205@126.com>
2022-03-04 23:50:53 +0800

    feat(Leetcode):力扣题目增加

---
+++ Leetcode/src/动态规划/Solution_198.java
@@ -0,0 +1,23 @@
+package 动态规划;
+
+/**
+ * @author liuke
+ * @date 2022/3/4 23:36
+ */
+public class Solution_198 {
+    public int rob(int[] nums) {
+        // 1.dp[i]代表投前i个房屋获得的最大价值
+        // 2.公式：dp[i] = max(dp[i-2] + nums[i], dp[i-1]);
+        // 3.初始值: dp[0] = nums[0], dp[1] = max(nums[0], nums[1])
+        if (nums.length == 1){
+            return nums[0];
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i-2] + nums[i], dp[i-1]);
+        }
+        return dp[nums.length - 1];
+    }
+}
